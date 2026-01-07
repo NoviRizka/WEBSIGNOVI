a96f30aa2f5c9a4418281dc98cee23d9781f4037
NoviRizka <novirizka78@gmail.com>
2026-01-07 10:27:50 +0700

    Add files via upload

---
+++ RumahSakit_1.js
@@ -0,0 +1,13 @@
+var json_RumahSakit_1 = {
+"type": "FeatureCollection",
+"name": "RumahSakit_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "pkuid": "1", "Rumah Sakit": "RSUD dr.R. Soetijono Blora", "Alamat": "Jl. Dr. Sutomo No.42, Tempelan, Kec. Blora, Kabupaten Blora, Jawa Tengah " }, "geometry": { "type": "Point", "coordinates": [ 111.425497916491111, -6.973264722094109 ] } },
+{ "type": "Feature", "properties": { "pkuid": "2", "Rumah Sakit": "RSU Permata Blora", "Alamat": "Jl. Reksodiputro Kel No.57, Jenar, Kedungjenar, Kec. Blora, Kabupaten Blora, Jawa Tengah" }, "geometry": { "type": "Point", "coordinates": [ 111.42064894709695, -7.002448334188644 ] } },
+{ "type": "Feature", "properties": { "pkuid": "3", "Rumah Sakit": "RSIA Annisa Blora", "Alamat": "Kunden, Kec. Blora, Kabupaten Blora, Jawa Tengah" }, "geometry": { "type": "Point", "coordinates": [ 111.40996325528387, -6.973444313553153 ] } },
+{ "type": "Feature", "properties": { "pkuid": "4", "Rumah Sakit": "RS PKU Muhammadiyah Blora", "Alamat": "Jalan Raya Jepon, Palkembar, Seso, Kec. Jepon, Kabupaten Blora, Jawa Tengah" }, "geometry": { "type": "Point", "coordinates": [ 111.471383534276669, -6.989966727785135 ] } },
+{ "type": "Feature", "properties": { "pkuid": "5", "Rumah Sakit": "RS PKU Muhammadiyah Cepu", "Alamat": "Jl. Ronggolawe No.137, Megal, Balun, Kec. Cepu, Kabupaten Blora, Jawa Tengah " }, "geometry": { "type": "Point", "coordinates": [ 111.559113962019325, -7.155819440211612 ] } },
+{ "type": "Feature", "properties": { "pkuid": "6", "Rumah Sakit": "RSUD dr.R. Soeprapto Cepu", "Alamat": "Jl. Ronggolawe No.50, Megal, Balun, Kec. Cepu, Kabupaten Blora, Jawa Tengah" }, "geometry": { "type": "Point", "coordinates": [ 111.574199644578968, -7.154382708539266 ] } }
+]
+}
